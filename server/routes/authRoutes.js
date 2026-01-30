a96458208e7e80733f00bdf2fab291f21d47a6b0
Harsh <kumarharsh93982@gmail.com>
2026-01-30 23:17:46 +0530

    Added JWT creating middleware.

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,11 +1,12 @@
 import express, { Router } from 'express';
 import signup from '../controllers/signup.js';
 import login from '../controllers/login.js';
+import { createToken } from '../middleware/authMiddleware.js';
 
 const userRoute = express.Router();
 
 userRoute.post('/signup', signup);
 
-userRoute.post('/login', login);
+userRoute.post('/login', createToken, login);
 
 export default userRoute;
