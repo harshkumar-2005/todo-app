27d7087dbb2205f7e8ada96df6121f3646ac194a
Harsh <kumarharsh93982@gmail.com>
2026-02-01 02:20:02 +0530

    Get all To-Do routes is introduced.

--- server/routes/todoRoutes.js
+++ server/routes/todoRoutes.js
@@ -3,6 +3,7 @@ import createTodo from '../controllers/createTodo.js';
 import deleteTodo from '../controllers/deleteTodo.js';
 import toggleStatus from '../controllers/todoStatusToggel.js';
 import { verifyToken } from '../middleware/authMiddleware.js'
+import getAllTodo from '../controllers/getAllTodo.js';
 
 const todoRouter = express.Router();
 
@@ -12,5 +13,6 @@ todoRouter.delete('/:id', verifyToken, deleteTodo);
 
 todoRouter.patch('/toggle/status/:id', verifyToken, toggleStatus);
 
+todoRouter.get('/get/all/todos', verifyToken, getAllTodo);
 
 export default todoRouter;
