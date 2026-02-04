a7577a6faa621799dd4e04b99f437c0b462fce71
Harsh <kumarharsh93982@gmail.com>
2026-02-04 15:46:07 +0530

    Removing createToken middleware and introducing a new route logout.

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,12 +1,14 @@
 import express, { Router } from 'express';
 import signup from '../controllers/signup.js';
 import login from '../controllers/login.js';
-import { createToken } from '../middleware/authMiddleware.js';
+import logout from '../controllers/logout.js'
 
 const userRoute = express.Router();
 
 userRoute.post('/signup', signup);
 
-userRoute.post('/login', createToken, login);
+userRoute.post('/login', login);
+
+userRoute.post('/logout', logout);
 
 export default userRoute;
